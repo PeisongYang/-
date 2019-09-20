d5a53e2197db049cfd8a5a293cece75cfd1d4740
杨培松 <582927335@qq.com>
2019-09-20 19:31:23 +0800

    上传至Github

---
+++ 16478090/app/src/main/java/com/example/myapplication/loginActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class loginActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
